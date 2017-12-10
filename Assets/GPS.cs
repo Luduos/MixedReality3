f78a47df2257880d80557026d763c6715a55266e
akashcastelino <32956844+akashcastelino@users.noreply.github.com>
2017-12-10 17:32:24 +0100

    Commit

---
+++ Assets/GPS.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS : MonoBehaviour {
+
+	public static GPS Instance{ set; get; }
+
+
+
+	public float lat,lon;
+	public bool isUnityRemote=true;
+
+	void Start()
+	{
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+		StartCoroutine(StartLocationService());
+	}
+
+	private IEnumerator StartLocationService(){
+		if (isUnityRemote)
+		{
+			yield return new WaitForSeconds(5);
+		}
+		if (!Input.location.isEnabledByUser) {
+			Debug.Log ("Switch on GPS");	
+			yield break;
+		}
+
+		Input.location.Start ();
+		if (isUnityRemote)
+		{
+			yield return new WaitForSeconds(5);
+		}
+		int maxWait = 20;
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
+			yield return new WaitForSeconds(1);
+			Debug.Log ("Waiting");
+			maxWait--;
+		}
+	
+
+		if (maxWait <= 0) {
+			Debug.Log ("TImed Out");
+			yield break;
+		}
+	
+		if (Input.location.status == LocationServiceStatus.Failed) {
+			Debug.Log ("failed");
+			yield break;}
+
+		lat = Input.location.lastData.latitude;
+		lon = Input.location.lastData.longitude;
+
+		yield break;
+
+	}
+
+}
---
+++ Assets/UpdateGPSTExt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPSTExt : MonoBehaviour {
+	public Text coordinates;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		coordinates.text = "Lat:" + GPS.Instance.lat.ToString () + "\n" + "Lon:" + GPS.Instance.lon.ToString ();
+	}
+}
