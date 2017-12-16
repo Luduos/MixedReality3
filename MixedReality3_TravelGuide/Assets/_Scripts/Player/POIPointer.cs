6344ea098c9354060a6682c33b3c03a5f4991a36
Luduos <david.liebemann@gmail.com>
2017-12-16 15:29:31 +0100

    Arrows are being shown and pointed correctly, as well as highlight of arrow that is being looked at

--- MixedReality3_TravelGuide/Assets/_Scripts/Player/POIPointer.cs
+++ MixedReality3_TravelGuide/Assets/_Scripts/Player/POIPointer.cs
@@ -1,16 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class POIPointer : MonoBehaviour {
+    [SerializeField]
+    private SpriteRenderer NormalSprite;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private SpriteRenderer BigSprite;
+
+    [SerializeField]
+    private float PointerDistanceToPlayer = 2.0f;
+
+    [SerializeField]
+    float DirectionMatchPercentage = 0.95f;
+
+    public Vector2 UnityTarget { get; set; }
+
+	public void OnPlayerPositionChanged(Vector2 PlayerPosition)
+    {
+        Vector3 dir = new Vector3(UnityTarget.x - PlayerPosition.x, UnityTarget.y - PlayerPosition.y, 0.0f);
+        if (dir.magnitude > 0.5f)
+        {
+            this.gameObject.SetActive(true);
+            dir = PointerDistanceToPlayer * dir.normalized;
+            this.transform.localPosition = dir;
+            this.transform.rotation = Quaternion.LookRotation(-Vector3.forward, dir);
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }     
+    }
+
+    public void OnPlayerRotationChanged(Vector2 forward)
+    {
+        float dotForwardTarget = Vector2.Dot(this.transform.up, forward);
+        if(dotForwardTarget > DirectionMatchPercentage)
+        {
+            BigSprite.gameObject.SetActive(true);
+            NormalSprite.gameObject.SetActive(false);
+        }
+        else
+        {
+            BigSprite.gameObject.SetActive(false);
+            NormalSprite.gameObject.SetActive(true);
+        }
+    }
+        
 }
--- MixedReality3_TravelGuide/Assets/_Scripts/Player/Player.cs
+++ MixedReality3_TravelGuide/Assets/_Scripts/Player/Player.cs
@@ -13,8 +13,6 @@ public class Player : MonoBehaviour {
     private GameObject PlayerModel;
 
     [SerializeField]
-    private float PointerDistanceToPlayer = 2.0f;
-    [SerializeField]
     private POIPointer PointerPrefab;
     private List<POIPointer> pointers = new List<POIPointer>();
 
@@ -27,7 +25,11 @@ public class Player : MonoBehaviour {
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        PlayerModel.transform.rotation = PlayerModel.transform.rotation * Quaternion.AngleAxis(Time.deltaTime * RotationSpeed * horizontalInput, -Vector3.forward);
+        if(horizontalInput != 0)
+        {
+            PlayerModel.transform.rotation = PlayerModel.transform.rotation * Quaternion.AngleAxis(Time.deltaTime * RotationSpeed * horizontalInput, -Vector3.forward);
+            SetLookAt(PlayerModel.transform.up);
+        }
     }
 
 
@@ -36,6 +38,18 @@ public class Player : MonoBehaviour {
         GPSPosition = gpsPosition;
         this.transform.position = MapInfo.instance.GetGPSAsUnityPosition(gpsPosition);
         this.transform.position += new Vector3(0.0f, 0.0f, -0.1f);
+        foreach(POIPointer poi in pointers)
+        {
+            poi.OnPlayerPositionChanged(gpsPosition);
+        }
+    }
+
+    public void SetLookAt(Vector2 lookat)
+    {
+        foreach (POIPointer poi in pointers)
+        {
+            poi.OnPlayerRotationChanged(lookat);
+        }
     }
 
     public void OnMapInfoLoaded()
@@ -45,10 +59,9 @@ public class Player : MonoBehaviour {
         {
             POIPointer pointer = Instantiate(PointerPrefab);
             pointer.transform.SetParent(this.transform, false);
-           
-            Vector3 dir = PointerDistanceToPlayer * new Vector3(poi.UnityPosition.x - CurrentPos.x, poi.UnityPosition.y - CurrentPos.y, 0.0f).normalized;
-            pointer.transform.Translate(dir.x, dir.y, -0.1f);
-            pointer.transform.LookAt(CurrentPos + dir);
+
+            pointer.UnityTarget = poi.UnityPosition;
+            pointer.OnPlayerPositionChanged(CurrentPos);
             pointers.Add(pointer);
         }
     }
