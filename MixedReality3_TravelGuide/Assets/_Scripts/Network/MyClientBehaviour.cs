0cfa0661ac8ffd481b8739c8bccabfbda196c65c
Luduos <david.liebemann@gmail.com>
2017-12-17 17:56:07 +0100

    testing messages

---
+++ MixedReality3_TravelGuide/Assets/_Scripts/Network/MyClientBehaviour.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+public class MyClientBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private Text DebugText;
+
+    private NetworkClient Client;
+
+
+    public class MyMsgType
+    {
+        public static short POIUpdate = MsgType.Highest + 1;
+    };
+
+    public class POIUpdateMessage : MessageBase
+    {
+        public int ID;
+    }
+
+    public void SetClient(NetworkClient client)
+    {
+        Client = client;
+        Client.RegisterHandler(MsgType.Connect, OnConnected);
+        Client.RegisterHandler(MyMsgType.POIUpdate, OnPOIUpdate);
+    }
+
+    public void SendPOIUpdate(int id)
+    {
+        POIUpdateMessage msg = new POIUpdateMessage();
+        msg.ID = id;
+        NetworkServer.SendToAll(MyMsgType.POIUpdate, msg);
+    }
+
+    public void OnPOIUpdate(NetworkMessage netMsg)
+    {
+        POIUpdateMessage msg = netMsg.ReadMessage<POIUpdateMessage>();
+        Debug.Log("ID " + msg.ID);
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnConnected(NetworkMessage msg)
+    {
+        Debug.Log("Connected to Server");
+        DebugText.text = "Jay";
+    }
+
+    private void OnPlayerConnected(NetworkPlayer player)
+    {
+        Debug.Log("Player connected to Server");
+
+    }
+
+    private void OnFailedToConnect(NetworkConnectionError error)
+    {
+        Debug.Log("Network Connection Error");
+    }
+}
---
+++ MixedReality3_TravelGuide/Assets/_Scripts/Network/MyNetworkBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(MyClientBehaviour))]
+public class MyNetworkBehaviour : NetworkDiscovery {
+
+    private NetworkClient currentHost;
+    private NetworkClient currentClient;
+
+    private MyClientBehaviour clientBehaviour;
+
+    private void Start()
+    {
+        clientBehaviour = this.gameObject.GetComponent<MyClientBehaviour>();
+        this.Initialize();
+        this.StartAsClient();
+    }
+
+    public void OnServeAsHost()
+    {
+        if(null == currentHost)
+        {
+            currentHost = NetworkManager.singleton.StartHost();
+            if(currentHost != null)
+            {
+                clientBehaviour.SetClient(currentHost);
+            }
+            this.isClient = false;
+            this.StartAsServer();
+        }
+        
+    }
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        if(currentClient == null)
+        {
+            NetworkManager.singleton.networkAddress = fromAddress;
+
+            currentClient = NetworkManager.singleton.StartClient();
+            if(currentClient != null)
+            {
+                clientBehaviour.SetClient(currentClient);
+                if (!this.isServer)
+                {
+                    this.enabled = false;
+                }
+            }
+        }
+    }
+}
